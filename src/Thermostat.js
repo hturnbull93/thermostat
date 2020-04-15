612f68d02519ca01a773af24d2115f4869bff796
Harry Turnbull <hturnbull@live.co.uk>
2020-04-15 15:16:17 +0100

    RF: magic numbers gone

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,18 +1,24 @@
 class Thermostat {
   constructor() {
-    this.temperature = 20;
+    this.DEFAULT_TEMP = 20;
+    this.temperature = this.DEFAULT_TEMP;
     this.powerSaving = true;
+    this.MINIMUM_TEMPERATURE = 10;
+    this.PS_ON_MAX_TEMP = 25;
+    this.PS_OFF_MAX_TEMP = 32;
+    this.LOW_USAGE_TEMP = 18;
+    this.HIGH_USAGE_TEMP = 24;
   }
   up() {
-    if(this.powerSaving && this.temperature <= 24){
+    if(this.powerSaving && this.temperature < this.PS_ON_MAX_TEMP){
       this.temperature++;
-    } else if(!this.powerSaving && this.temperature <= 31) {
+    } else if(!this.powerSaving && this.temperature < this.PS_OFF_MAX_TEMP) {
       this.temperature++;
     };
   };
 
   down() {
-    if(this.temperature > 10){
+    if(this.temperature > this.MINIMUM_TEMPERATURE){
       this.temperature--;
     }
   };
@@ -22,13 +28,13 @@ class Thermostat {
   };
 
   reset() {
-    this.temperature = 20;
+    this.temperature = this.DEFAULT_TEMP;
   };
 
   energyUsage() {
-    if(this.temperature <18){
+    if(this.temperature < this.LOW_USAGE_TEMP){
       return "low-usage"
-    } else if(this.temperature > 24) {
+    } else if(this.temperature > this.HIGH_USAGE_TEMP) {
       return "high-usage"
     } else {
       return "medium-usage"
