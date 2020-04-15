84859b7179a980c0d23dc439dbb1ed0dd77a7ac4
Harry Turnbull <hturnbull@live.co.uk>
2020-04-15 16:22:55 +0100

    RF: interface now in interface.js

---
+++ interface.js
@@ -0,0 +1,34 @@
+var thermostat = new Thermostat();
+
+$(function () {
+
+  $('#temperature').html(thermostat.temperature);
+  $('#power-saving').html(thermostat.powerSavingStatus());
+  $('#energy-usage').html(thermostat.energyUsage());
+
+  $("#up").click(function () {
+    thermostat.up();
+    updateTemp();
+  });
+
+  $("#down").click(function () {
+    thermostat.down();
+    updateTemp();
+  });
+
+  $("#reset").click(function () {
+    thermostat.reset();
+    updateTemp();
+  });
+
+  $("#toggle-power-saving").click(function () {
+    thermostat.togglePowerSaving();
+    $('#power-saving').html(thermostat.powerSavingStatus());
+  });
+
+  var updateTemp = () => {
+    $('#temperature').html(thermostat.temperature);
+    $('#energy-usage').html(thermostat.energyUsage());
+  }
+
+});
