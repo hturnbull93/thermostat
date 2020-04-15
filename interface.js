22e3c193923fcd93bd3ac232c4b0cfae1190ca59
Edward Phillips <edd0ddi0@gmail.com>
2020-04-15 17:43:07 +0100

    did some Jquery/ajax stuff

--- interface.js
+++ interface.js
@@ -1,5 +1,6 @@
 var thermostat = new Thermostat();
-
+const TOKEN = '&APPID=eb6ae52da19175e8885ca553f8c4f3e7';
+const APIURL = 'http://api.openweathermap.org/data/2.5/forecast?q='
 $(function () {
 
   $('#temperature').html(thermostat.temperature);
@@ -27,11 +28,22 @@ $(function () {
     thermostat.togglePowerSaving();
     $('#power-saving').html(thermostat.powerSavingStatus());
   });
+  
+  $("#select-city").submit(function(event) {
+    event.preventDefault();
+    var city = $("#current-city").val();
+    displayWeather(city);
+  });
 
   var updateTemp = () => {
     $('#temperature').html(thermostat.temperature);
     $('#energy-usage').html(thermostat.energyUsage());
     $('#temperature').attr('class', thermostat.energyUsage());
-  }
+  };
 
+  var displayWeather = (city) => {
+    $.get(APIURL + city + TOKEN, function(result) {
+      $('#weather-report').html(result.list[0].weather[0].description)
+    });
+  };
 });
