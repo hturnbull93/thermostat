756ba9b6f23b8989c14a89ac4b395890c2909825
Edward Phillips <edd0ddi0@gmail.com>
2020-04-15 15:09:38 +0100

    tests - various - passing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -25,15 +25,48 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toEqual(10);
   });
 
-  it('powersaving mode should be on by default', () => {
+  it('power saving mode should be on by default', () => {
     expect(thermostat.powerSaving).toEqual(true);
   });
 
-  xit('temperature should not go above 25 degrees when power saving mode is on', () => {
+  it('temperature should not go above 25 degrees when power saving mode is on', () => {
     thermostat.powersaving
     for (let index = 0; index < 6; index++) {
       thermostat.up();
     };
     expect(thermostat.temperature).toEqual(25);
   });
+
+  it('temperature should not go above 32 degress when power saving mode is off', () => {
+    thermostat.togglePowerSaving();
+    for (let index = 0; index < 13; index++) {
+      thermostat.up();
+    };
+    expect(thermostat.temperature).toEqual(32);
+  });
+
+  it('should be able to reset the temperature to 20', () => {
+    thermostat.up();
+    thermostat.reset();
+    expect(thermostat.temperature).toEqual(20)
+  });
+
+  it('should be able to tell me if the energy usage is low', () => {
+    for (let index = 0; index < 3; index++) {
+      thermostat.down();
+    };
+    expect(thermostat.energyUsage()).toEqual("low-usage")
+  });
+  
+  it('should be able to tell me if the energy usage is mid-level', () => {
+    expect(thermostat.energyUsage()).toEqual("medium-usage")
+  });
+
+  it('should be able to tell me if the energy usage is high', () => {
+    for (let index = 0; index < 5; index++) {
+      thermostat.up();
+    };
+    expect(thermostat.energyUsage()).toEqual("high-usage")
+  });
+
 }) 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -4,13 +4,35 @@ class Thermostat {
     this.powerSaving = true;
   }
   up() {
-    this.temperature++;
+    if(this.powerSaving && this.temperature <= 24){
+      this.temperature++;
+    } else if(!this.powerSaving && this.temperature <= 31) {
+      this.temperature++;
+    };
   };
+
   down() {
     if(this.temperature > 10){
       this.temperature--;
     }
   };
 
-}
+  togglePowerSaving() {
+    this.powerSaving = !this.powerSaving;
+  };
+
+  reset() {
+    this.temperature = 20;
+  };
+
+  energyUsage() {
+    if(this.temperature <18){
+      return "low-usage"
+    } else if(this.temperature > 24) {
+      return "high-usage"
+    } else {
+      return "medium-usage"
+    }
+  }
+};
 
